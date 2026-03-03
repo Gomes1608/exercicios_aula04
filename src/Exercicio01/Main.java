bf98a6e065e09d2a2e2c24efb93b82fbb5219c31
Laboratório FIAP <guiguigcarneiro@gmail.com>
2026-03-03 11:32:14 -0300

    continuação do exercicio

--- src/Exercicio01/Main.java
+++ src/Exercicio01/Main.java
@@ -1,7 +1,26 @@
 package Exercicio01;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String nome;
+        double valor;
+        int qtd;
+        double porcentagem;
 
+        System.out.println("Nome do produto: ");
+        nome = sc.next();
+        System.out.println("Valor do produto em R$: ");
+        valor = sc.nextDouble();
+        System.out.println("Quantidade inicial em estoque: ");
+        qtd = sc.nextInt();
+
+        Produto produto = new Produto(nome,valor,qtd);
+        System.out.println("Qual a porcentagem do aumento? ");
+        porcentagem=sc.nextDouble();
+
+        produto.aumentarValor(porcentagem);
     }
 }
--- src/Exercicio01/Produto.java
+++ src/Exercicio01/Produto.java
@@ -4,4 +4,13 @@ public class Produto {
     String nome;
     double valor;
     int qtd;
+
+    public Produto(String nome, double valor, int qtd){
+        this.nome = nome;
+        this.valor = valor;
+        this.qtd = qtd;
+    }
+    public void aumentarValor(double porcentagem){
+        valor *= (1+porcentagem/100);
+    }
 }
